d95af3ea51c7a4dc4bdc5f928a7debb859fb9fd5
amelol <75670435+amelol@users.noreply.github.com>
2021-04-29 21:35:38 +0200

    add pacman

--- Assets/js/models/game.js
+++ Assets/js/models/game.js
@@ -9,7 +9,7 @@ class Game {
     this.ctx = canvas.getContext("2d")
 
     this.background = new Background(this.ctx)
-    // this.pacman = new Pacman(this.ctx)
+    this.pacman = new Pacman(this.ctx)
 
     // this.obstacles = [
     //   new Obstacle(this.ctx)
@@ -27,62 +27,31 @@ class Game {
       this.clear()
       this.move()
       this.draw()
-      // this.checkCollisions()
-      // if (this.drawCount > 200) {
-      //   this.drawCount = 0
-      //   this.addObstacle()
-      // }
+      if (this.drawCount > 200) {
+        this.drawCount = 0
+      }
     }, 1000 / 60)
   }
-
-  // checkCollisions() {
-  //   const collision = this.obstacles.some(obstacle => {
-  //     const colX = (
-  //       this.mario.x + this.mario.w >= obstacle.x &&
-  //       this.mario.x <= obstacle.x + obstacle.w
-  //     )
-
-  //     const colY = this.mario.y + this.mario.h >= obstacle.y
-
-  //     return colX && colY
-  //   })
-
-  //   if (collision) {
-  //     this.stop()
-  //   }
-  // }
 
   stop() {
     clearInterval(this.intervalId)
   }
 
   onKeyEvent(event) {
-  //   this.pacman.onKeyEvent(event)
+    this.pacman.onKeyEvent(event)
   }
 
   clear() {
     this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height)
-
-    // this.obstacles = this.obstacles.filter(o => {
-    //   return o.x + o.w >= 0
-    // })
   }
 
   draw() {
     this.drawCount++
     this.background.draw()
-    // this.pacman.draw()
-    //this.obstacles.forEach(o => o.draw())
+    this.pacman.draw()
   }
 
   move() {
     // this.pacman.move()
-    // this.obstacles.forEach(o => o.move())
   }
-
-  // addObstacle() {
-  //   const newObstacle = new Obstacle(this.ctx)
-
-  //   this.obstacles.push(newObstacle)    
-  // }
 }
---
+++ Assets/js/models/pacman.js
@@ -0,0 +1,48 @@
+class Pacman {
+
+  constructor(ctx) {
+    this.ctx = ctx
+
+    this.x = 2
+    this.y = 460
+    this.y0 = this.y
+
+    this.vx = 0
+    this.vy = 0
+
+    this.w = 55
+    this.h = 55
+    this.h0 = this.h
+
+    this.img = new Image()
+    this.img.drawCount = 0
+    this.img.frames = 3
+    this.img.frameIndex = 0
+    this.img.src = './assets/img/pacman.png'
+  }
+
+
+  onKeyEvent(event) {
+  }
+
+  draw() {
+    this.img.drawCount++
+
+    if (this.img.drawCount >= 10) {
+      this.img.drawCount = 0
+    }
+
+    this.ctx.drawImage(
+      this.img,
+      this.img.frameIndex * this.img.width / this.img.frames,
+      0,
+      this.img.width / 3,
+      this.img.height,
+      this.x,
+      this.y,
+      this.w,
+      this.h
+    )
+  }
+
+}
